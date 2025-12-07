7460061fed46bbac43d4b59558a667ed637d7f2b
xsb0730 <570610221@qq.com>
2025-12-07 11:49:33 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    repositories {
+        // 配置插件仓库（用于下载Gradle插件，如Android、Kotlin插件）
+        maven { url = uri("https://mirrors.cloud.tencent.com/maven/") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/gradle/") }
+
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+
+        maven { url = uri("https://jitpack.io")}
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS) // 强制优先使用这里的仓库，禁止项目级配置
+    repositories {
+        // 优先国内镜像，替代官方源
+        maven { url = uri("https://mirrors.cloud.tencent.com/maven/") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/gradle/") }
+
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+
+        maven { url = uri("https://jitpack.io") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ClassWork4"
+include(":app")
+ 
